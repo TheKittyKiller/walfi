66e46b8e520bb70b71b09471b3478d3c79ea577e
sebastian melph <jame1307@stud.kea.dk>
2022-12-20 18:03:50 +0100

    carosouel

--- product-page.js
+++ product-page.js
@@ -18,7 +18,26 @@ function showProducts(products) {
 
 
 
+const buttons = document.querySelectorAll("[data-caro-button]")
 
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+
+    const offset = button.dataset.caroButton === "next" ? 1 : -1
+    const slides = button
+    .closest("[data-caro]")
+    .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex =  [...slides.children].indexOf(activeSlide) + offset
+if (newIndex < 0 ) newIndex = slides.children.length - 1
+if (newIndex >= slides.children.length) newIndex = 0
+
+slides.children[newIndex].dataset.active = true 
+delete activeSlide.dataset.active
+
+  })
+})
 
 
 // function buttonColor() { 
@@ -33,9 +52,9 @@ function showProducts(products) {
 
 
 
-let button= document.querySelector('#button');
+// let button= document.querySelector('#button');
 
-button.addEventListener('click', ()=> button.style.backgroundColor ='grey') 
+// button.addEventListener('click', ()=> button.style.backgroundColor ='grey') 
 
 
 
@@ -49,6 +68,3 @@ button.addEventListener('click', ()=> button.style.backgroundColor ='grey')
 // button.style.backgroundColor= colors[index]; 
 // index= index + 1 
 // }); 
-
-
-
